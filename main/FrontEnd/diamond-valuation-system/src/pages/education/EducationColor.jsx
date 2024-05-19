d52f48c2a794082f4d86fa11252c43d7d9a08232
Lâm Tiên Hưng <89993051+gs4lthung@users.noreply.github.com>
2024-05-19 16:18:00 +0700

    create a shared folder and move data to it

--- main/FrontEnd/diamond-valuation-system/src/pages/education/EducationColor.jsx
+++ main/FrontEnd/diamond-valuation-system/src/pages/education/EducationColor.jsx
@@ -23,87 +23,17 @@ import React, { useState } from "react";
 import EducationTitle from "../../components/EducationTitle";
 import { IoDiamondOutline } from "react-icons/io5";
 import EducationBottomLineRec from "../../components/EducationBottomLineRec";
-
+import {
+  type,
+  title,
+  content,
+  image,
+  typeTable,
+  desTable,
+  whatToLookFor,
+  whatToAvoid,
+} from "../../shared/SharedEducationColor";
 export default function EducationColor() {
-  const type = ["D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N-Z"];
-  const title = [
-    "D Color Diamonds (Absolutely Colorless)",
-    "E Color Diamond (Colorless)",
-    "F Color Diamond (Colorless)",
-    "G Color Diamond (Near Colorless)",
-    "H Color Diamond (Near Colorless)",
-    "I Color Diamond (Near Colorless)",
-    "J Color Diamond (Near Colorless)",
-    "K Color Diamond (Faint)",
-    "L Color Diamonds (Faint to Light Color)",
-    "M Color Diamonds (Faint to Light Color)",
-    "N-Z Color Diamonds (Faint to Light Color)",
-  ];
-  const content = [
-    "D is the highest color grade, meaning it has nearly no color. Under magnification and to the naked eye, a D color diamond will appear colorless. D color diamonds are usually set in platinum or white gold, as yellow gold and other jewelry settings detract from the diamond’s uncolored beauty. Diamonds with a D color grade are the most rare and expensive on the market, with a significant price premium over other color grades.",
-    "E color diamonds look almost identical to D color diamonds. Most of the time, the differences in color between a D and E diamond are only visible to an expert gemologist when the two diamonds are viewed under magnification. Like D color diamonds, E color diamonds are usually set in platinum or white gold to avoid the color of the jewelry detracting from their near flawless color. Although these diamonds are less expensive than D color diamonds, they still command a hefty premium.",
-    "F color diamonds are almost identical to D and E color diamonds, with nearly no visible color. Even under magnification and side by side, a D, E and F diamond will look almost identical to anyone other than an expert gemologist.",
-    "G color diamonds exhibit nearly no color and appear primarily colorless to the naked eye. The G color grade is the highest, best grade in the “Near Colorless” range of the GIA’s scale, which covers diamonds graded G to J. Although G color diamonds have some tints of color, they’re almost impossible to detect with the naked eye. Like D-F diamonds, these diamonds should be set in platinum or white gold to reduce any effects of color reflection from yellow or rose gold.",
-    "H color diamonds appear primarily colorless to the naked eye but have a faint yellow hue that’s often visible under magnification in bright lighting, especially when they’re compared to diamonds of a higher color grade. Like G color diamonds, these can be set in platinum or white gold without any issues. H color diamonds are slightly less expensive than G color diamonds and significantly more affordable than diamonds in the colorless range. This is the minimum color grade we recommend for diamond shapes with a large table, such as the radiant and cushion cuts.",
-    "I color diamonds offer a great combination of near colorless looks and good value for money. These diamonds have a slight yellow tint that’s usually only visible when they’re viewed next to diamonds of a higher color grade. An I color round brilliant cut diamond can look fantastic in a platinum or white gold setting, as well as alongside metals such as yellow or rose gold. As you’d expect, I color diamonds are less expensive than G or H color diamonds. This is the minimum color grade we recommend for princess cut diamonds.",
-    "J color diamonds look mostly colorless to the naked eye, but usually have a faint yellow tint that’s easy to notice under bright lights and magnification. In diamonds with a large table, the color might also be visible with the naked eye in certain lighting conditions. From a value for money perspective, J color diamonds can be fantastic choices. We recommend this color for round brilliant cut diamonds set in platinum or white gold (solitaire setting), as the cut of the round diamond is great at concealing color. However, the J color grade is not recommended for diamond shapes with a larger table and fewer facets.",
-    "K color diamonds are classed as “faint tint” on the GIA’s diamond color scale, meaning they have a slight yellow tint that’s visible even to the naked eye. Diamonds in this price range can be found for significantly less than those in the G to J range. Set in a beautiful yellow gold setting, a K color round brilliant cut diamond can look gorgeous. However, diamonds of this color grade should generally not be set in platinum or white gold.",
-    "L color diamonds have a yellow tint that’s visible to the naked eye in normal lighting conditions. Diamonds with this color grade are much more affordable than those in the G to J range, making them a good value for money option. Because of the yellow tint, we don’t recommend the L color grade for non-round diamond shapes. We also don’t recommend it for diamonds set in platinum or white gold. However, L color round brilliant cut diamonds can still look great in yellow gold solitaire rings due to the warm, yellow tone of the metal.",
-    "M color diamonds have a definite yellow tint that’s visible to the naked eye. Like K and L diamonds, M color diamonds offer fantastic value for money when compared to near colorless or colorless diamonds. The M color grade is typically the lowest color grade offered by online diamond vendors. Although M color diamonds can look warm and beautiful in antique yellow gold settings, their color is quite easy to notice even with the naked eye.",
-    "Diamonds in the N to Z range have noticeable yellow or brown tinting. These diamonds are available at a much lower price point than faintly tinted or near colorless diamonds. We do not recommend diamonds of an N-Z grade.",
-  ];
-  const image = [
-    "https://www.diamonds.pro/wp-content/uploads/2023/08/ColorSlider_D.jpeg",
-    "https://www.diamonds.pro/wp-content/uploads/2023/08/ColorSlider_E.jpeg",
-    "https://www.diamonds.pro/wp-content/uploads/2023/08/ColorSlider_F.jpeg",
-    "https://www.diamonds.pro/wp-content/uploads/2023/08/ColorSlider_G.jpeg",
-    "https://www.diamonds.pro/wp-content/uploads/2023/08/ColorSlider_H.jpeg",
-    "https://www.diamonds.pro/wp-content/uploads/2023/08/ColorSlider_I.jpeg",
-    "https://www.diamonds.pro/wp-content/uploads/2023/08/ColorSlider_J.jpeg",
-    "https://www.diamonds.pro/wp-content/uploads/2023/08/ColorSlider_K.jpeg",
-    "https://www.diamonds.pro/wp-content/uploads/2023/08/M.jpg",
-    "https://www.diamonds.pro/wp-content/uploads/2023/08/M-1.jpg",
-    "https://www.diamonds.pro/wp-content/uploads/2023/08/N-Z.jpg",
-  ];
-
-  const typeTable = [
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N-R",
-    "S-Z",
-  ];
-  const desTable = [
-    "D is the highest color grade, meaning it has nearly no color. Under magnification and to the naked eye, a D color diamond will appear colorless.<br/> D color diamonds are usually set in platinum or white gold, as yellow gold and other jewelry settings detract from the diamond’s uncolored beauty. Diamonds with a D color grade are the most rare and expensive on the market, with a significant price premium over other color grades.",
-    "E color diamonds look almost identical to D color diamonds. Most of the time, the differences in color between a D and E diamond are only visible to an expert gemologist when the two diamonds are viewed under magnification.<br/> Like D color diamonds, E color diamonds are usually set in platinum or white gold to avoid the color of the jewelry detracting from their near flawless color. Although these diamonds are less expensive than D color diamonds, they still command a hefty premium.",
-    "F color diamonds are almost identical to D and E color diamonds, with nearly no visible color. Even under magnification and side by side, a D, E and F diamond will look almost identical to anyone other than an expert gemologist.",
-    "G color diamonds exhibit nearly no color and appear primarily colorless to the naked eye. The G color grade is the highest, best grade in the “Near Colorless” range of the GIA’s scale, which covers diamonds graded G to J.<br/> Although G color diamonds have some tints of color, they’re almost impossible to detect with the naked eye. Like D-F diamonds, these diamonds should be set in platinum or white gold to reduce any effects of color reflection from yellow or rose gold. color diamonds look almost identical to D color diamonds. Most of the time, the differences in color between a D and E diamond are only visible to an expert gemologist when the two diamonds are viewed under magnification. Like D color diamonds, E color diamonds are usually set in platinum or white gold to avoid the color of the jewelry detracting from their near flawless color. Although these diamonds are less expensive than D color diamonds, they still command a hefty premium.",
-    "H color diamonds appear primarily colorless to the naked eye but have a faint yellow hue that’s often visible under magnification in bright lighting, especially when they’re compared to diamonds of a higher color grade.<br/> Like G color diamonds, these can be set in platinum or white gold without any issues. H color diamonds are slightly less expensive than G color diamonds and significantly more affordable than diamonds in the colorless range. This is the minimum color grade we recommend for diamond shapes with a large table, such as the radiant and cushion cuts.",
-    "I color diamonds offer a great combination of near colorless looks and good value for money. These diamonds have a slight yellow tint that’s usually only visible when they’re viewed next to diamonds of a higher color grade.<br/> An I color round brilliant cut diamond can look fantastic in a platinum or white gold setting, as well as alongside metals such as yellow or rose gold. As you’d expect, I color diamonds are less expensive than G or H color diamonds. This is the minimum color grade we recommend for princess cut diamonds.",
-    "J color diamonds look mostly colorless to the naked eye, but usually have a faint yellow tint that’s easy to notice under bright lights and magnification. In diamonds with a large table, the color might also be visible with the naked eye in certain lighting conditions.<br/> From a value for money perspective, J color diamonds can be fantastic choices. We recommend this color for round brilliant cut diamonds set in platinum or white gold (solitaire setting), as the cut of the round diamond is great at concealing color. However, the J color grade is not recommended for diamond shapes with a larger table and fewer facets.",
-    "K color diamonds are classed as “faint tint” on the GIA’s diamond color scale, meaning they have a slight yellow tint that’s visible even to the naked eye.<br/> Diamonds in this price range can be found for significantly less than those in the G to J range. Set in a beautiful yellow gold setting, a K color round brilliant cut diamond can look gorgeous. However, diamonds of this color grade should generally not be set in platinum or white gold.",
-    "L color diamonds have a yellow tint that’s visible to the naked eye in normal lighting conditions. Diamonds with this color grade are much more affordable than those in the G to J range, making them a good value for money option.<br/> Because of the yellow tint, we don’t recommend the L color grade for non-round diamond shapes. We also don’t recommend it for diamonds set in platinum or white gold. However, L color round brilliant cut diamonds can still look great in yellow gold solitaire rings due to the warm, yellow tone of the metal.",
-    "M color diamonds have a definite yellow tint that’s visible to the naked eye. Like K and L diamonds, M color diamonds offer fantastic value for money when compared to near colorless or colorless diamonds.<br/> The M color grade is typically the lowest color grade offered by online diamond vendors. Although M color diamonds can look warm and beautiful in antique yellow gold settings, their color is quite easy to notice even with the naked eye.",
-    "Diamonds in the N to R range have noticeable yellow or brown tinting. These diamonds are available at a much lower price point than faintly tinted or near colorless diamonds. We do not recommend diamonds of an N-R grade.",
-    "Diamonds of an S-Z range have easily noticeable yellow or brown tinting. For this reason, We do not recommend S-Z diamonds.",
-  ];
-  const whatToLookFor = [
-    "Find the sweet spot between value and beauty. In most cases, this comes in the “near colorless” range, from G to J.",
-    "Pair diamonds with a low color grade with a yellow or rose gold setting to make it appear whiter.",
-    "Make sure the color grade comes from a reputable grading laboratory, like the GIA.",
-  ];
-  const whatToAvoid = [
-    "Don’t go too high in color. This can result in overpaying for a grade that looks similar to one several grades lower.",
-    "Avoid choosing a setting with accent diamonds that are significantly higher or lower in color than the center diamond.",
-    "Be careful of high color grades with a high level of fluorescence, as this can give it a hazy appearance.",
-  ];
   const [sliderValue, setSliderValue] = useState(50);
 
   return (
