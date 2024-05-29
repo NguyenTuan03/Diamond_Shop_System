203ba4d0f3e9baeb064ed8db2f49b872e55ac37a
Lâm Tiên Hưng <89993051+gs4lthung@users.noreply.github.com>
2024-05-29 17:04:44 +0700

    update

--- main/BackEnd/diamond_shop/src/main/java/com/diamond_shop/diamond_shop/config/SecurityConfig.java
+++ main/BackEnd/diamond_shop/src/main/java/com/diamond_shop/diamond_shop/config/SecurityConfig.java
@@ -18,7 +18,11 @@ public class SecurityConfig {
                 .authorizeRequests(authorizeRequests ->
                         authorizeRequests
                                 .requestMatchers("/api/account/save").permitAll()
+<<<<<<< Updated upstream
                                 .requestMatchers(HttpMethod.POST, "/api/account/login").permitAll()
+=======
+                                .requestMatchers(HttpMethod.POST,"/api/account/login").permitAll()
+>>>>>>> Stashed changes
                                 .requestMatchers(HttpMethod.POST, "/api/diamond/diamond-calculate").permitAll()
                                 .anyRequest().permitAll() // Allow access without authentication to all requests
                 )
