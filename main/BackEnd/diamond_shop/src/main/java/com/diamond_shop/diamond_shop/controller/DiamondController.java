7714d032665173bc971e4759627ea549443f6e8e
Lâm Tiên Hưng <89993051+gs4lthung@users.noreply.github.com>
2024-05-29 16:57:41 +0700

    fix: cannot call API from frontend to calculate Diamond price
    - Enable cors for "/api/diamond/diamond-calculate".
    - Add gson dependency

--- main/BackEnd/diamond_shop/src/main/java/com/diamond_shop/diamond_shop/controller/DiamondController.java
+++ main/BackEnd/diamond_shop/src/main/java/com/diamond_shop/diamond_shop/controller/DiamondController.java
@@ -2,6 +2,7 @@ package com.diamond_shop.diamond_shop.controller;
 
 import com.diamond_shop.diamond_shop.dto.DiamondCheckRequestDTO;
 import com.diamond_shop.diamond_shop.service.DiamondService;
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -11,20 +12,20 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin
 @RequestMapping("api/diamond")
 public class DiamondController {
-
     @Autowired
     private DiamondService diamondService;
 
     @PostMapping(value = "/diamond-calculate", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> getDiamondAttr(@RequestBody DiamondCheckRequestDTO request) {
-        String gradingLab = request.getGrading_Lab();
+        String gradingLab = request.getGradingLab();
         String carat = request.getCarat();
         String shape = request.getShape();
         String color = request.getColor();
         String clarity = request.getClarity();
         String cut = request.getCut();
-        ResponseEntity<String> result=diamondService.fetchDiamondCalculate(gradingLab, carat, shape, color, clarity, cut);
+        ResponseEntity<String> result = diamondService.fetchDiamondCalculate(gradingLab, carat, shape, color, clarity, cut);
         System.out.println(result);
-        return result;
+        Gson gson = new Gson();
+        return ResponseEntity.ok(gson.toJson(result));
     }
 }
