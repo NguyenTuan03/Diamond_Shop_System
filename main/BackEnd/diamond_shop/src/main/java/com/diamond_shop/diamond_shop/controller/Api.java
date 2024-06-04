4caff38ca9b6e215d264f48a50abfd7ead6c1df4
NguyenTuan03 <tuannase170164@fpt.edu.vn>
2024-06-04 04:57:27 +0200

    Fix servicEntity

--- main/BackEnd/diamond_shop/src/main/java/com/diamond_shop/diamond_shop/controller/Api.java
+++ main/BackEnd/diamond_shop/src/main/java/com/diamond_shop/diamond_shop/controller/Api.java
@@ -10,9 +10,13 @@ import com.diamond_shop.diamond_shop.service.AccountService;
 import com.diamond_shop.diamond_shop.service.ProcessRequestService;
 import com.diamond_shop.diamond_shop.service.ValuationRequestService;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -49,5 +53,6 @@ public class Api {
         processRequestService.processRequest();
         return "Request Successfully!!";
     }
+
     
 }
