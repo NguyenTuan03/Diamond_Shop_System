c5e803a97b48f1e0ff1ce3dcfa10c6b2b94b5b11
Lâm Tiên Hưng <89993051+gs4lthung@users.noreply.github.com>
2024-06-17 22:16:57 +0700

    test: login test

--- main/BackEnd/diamond_shop/src/test/java/diamond_shop/diamond_shop/DiamondShopApplicationTests.java
+++ main/BackEnd/diamond_shop/src/test/java/diamond_shop/diamond_shop/DiamondShopApplicationTests.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@SpringBootConfiguration
 class DiamondShopApplicationTests {
 
 	@Test
