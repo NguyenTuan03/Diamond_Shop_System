e40a1156fcaf60230b4f2022e934747062eeab11
Lâm Tiên Hưng <89993051+gs4lthung@users.noreply.github.com>
2024-06-18 22:46:06 +0700

    fix: validation

--- main/BackEnd/diamond_shop/src/main/java/com/diamond_shop/diamond_shop/entity/RoleEntity.java
+++ main/BackEnd/diamond_shop/src/main/java/com/diamond_shop/diamond_shop/entity/RoleEntity.java
@@ -3,7 +3,6 @@ package com.diamond_shop.diamond_shop.entity;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
