c422fbf8f71c5b12f7b6c5232d440c5ee9637203
Lâm Tiên Hưng <89993051+gs4lthung@users.noreply.github.com>
2024-06-19 17:47:54 +0700

    feat: Sealing letter functions for manager

--- main/BackEnd/diamond_shop/src/main/java/com/diamond_shop/diamond_shop/repository/SealingLetterRepository.java
+++ main/BackEnd/diamond_shop/src/main/java/com/diamond_shop/diamond_shop/repository/SealingLetterRepository.java
@@ -1,9 +1,16 @@
 package com.diamond_shop.diamond_shop.repository;
 
 import com.diamond_shop.diamond_shop.entity.SealingLetterEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SealingLetterRepository extends JpaRepository<SealingLetterEntity, Integer> {
+public interface SealingLetterRepository extends JpaRepository<SealingLetterEntity, Integer>, PagingAndSortingRepository<SealingLetterEntity, Integer> {
+
+    @Query(value = "SELECT s FROM SealingLetterEntity s")
+    Page<SealingLetterEntity> findAll(Pageable pageable);
 }
