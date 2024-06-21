342f193cfc11ae96c5c7dc5511f30f6bc30b9809
Lâm Tiên Hưng <89993051+gs4lthung@users.noreply.github.com>
2024-06-21 07:30:40 +0700

    feat: make valuation receipt

--- main/BackEnd/diamond_shop/src/main/java/com/diamond_shop/diamond_shop/controller/ValuationReceiptController.java
+++ main/BackEnd/diamond_shop/src/main/java/com/diamond_shop/diamond_shop/controller/ValuationReceiptController.java
@@ -1,8 +1,11 @@
 package com.diamond_shop.diamond_shop.controller;
 
+import com.diamond_shop.diamond_shop.entity.ValuationReceiptEntity;
 import com.diamond_shop.diamond_shop.service.ValuationReceiptService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @CrossOrigin
@@ -12,9 +15,14 @@ public class ValuationReceiptController {
     @Autowired
     private ValuationReceiptService valuationReceiptService;
 
-    @GetMapping(path = "/get")
+    @GetMapping(path = "/get/all")
     public String getAllValuationReceipt() {
 
         return null;
+    }
+
+    @GetMapping(path = "/get")
+    public Optional<ValuationReceiptEntity> getByValuationRequestId(@RequestParam("valuation-request-id") int valuationRequestId) {
+        return valuationReceiptService.findByValuationRequestId(valuationRequestId);
     }
 }
--- main/BackEnd/diamond_shop/src/main/java/com/diamond_shop/diamond_shop/pojo/ValuationReceiptPojo.java
+++ main/BackEnd/diamond_shop/src/main/java/com/diamond_shop/diamond_shop/pojo/ValuationReceiptPojo.java
@@ -4,12 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Date;
+
 @Getter
 @Setter
 @AllArgsConstructor
 public class ValuationReceiptPojo {
+    private int id;
+    private Date createdDate;
+    private int customerId;
     private String customerName;
     private String customerPhoneNumber;
-    private String customerAddress;
+    private String serviceName;
     private String price;
+    private Date valuationFinishDate;
+    private Date valuationSealingDate;
 }
