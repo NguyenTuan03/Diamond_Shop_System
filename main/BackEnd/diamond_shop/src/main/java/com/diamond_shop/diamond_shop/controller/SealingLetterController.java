7230baaf9ab3585814a9462bd5a788ddc866d203
Lâm Tiên Hưng <89993051+gs4lthung@users.noreply.github.com>
2024-06-26 17:14:38 +0700

    feat: create sealing letter for manager

--- main/BackEnd/diamond_shop/src/main/java/com/diamond_shop/diamond_shop/controller/SealingLetterController.java
+++ main/BackEnd/diamond_shop/src/main/java/com/diamond_shop/diamond_shop/controller/SealingLetterController.java
@@ -4,23 +4,23 @@ import com.diamond_shop.diamond_shop.entity.SealingLetterEntity;
 import com.diamond_shop.diamond_shop.service.SealingLetterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
 @RequestMapping("api/sealing-letter")
 public class SealingLetterController {
-//    @Autowired
-//    private SealingLetterService sealingLetterService;
-//
-//    @GetMapping(path = "/check", produces = MediaType.APPLICATION_JSON_VALUE)
-//    public String checkSealingDate(@RequestParam("id") int id) {
-//        return sealingLetterService.checkSealingDate(id);
-//    }
-//
-//    @GetMapping(path = "/get/all")
-//    public Page<SealingLetterEntity> getAllSealing(@RequestParam("page") int page) {
-//        return sealingLetterService.getAllSealingLetters(page);
-//    }
+    @Autowired
+    private SealingLetterService sealingLetterService;
+
+    @GetMapping("/get/all")
+    public Page<SealingLetterEntity> getAllSealingLetters(@RequestParam("page") int page) {
+        return null;
+    }
+
+    @PostMapping("/create")
+    public String createSealingLetterByValuationRequestId(@RequestParam int valuationRequestId) {
+        return sealingLetterService.createSealingLetter(valuationRequestId);
+    }
+
 }
