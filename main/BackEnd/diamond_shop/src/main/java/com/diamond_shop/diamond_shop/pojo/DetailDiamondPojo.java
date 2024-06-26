808d6601699eee17c2fc3fa075ecc4a9b9cdedc9
NguyenTuan03 <tuannase170164@fpt.edu.vn>
2024-06-26 12:56:00 +0700

    Add detail Diamond

---
+++ main/BackEnd/diamond_shop/src/main/java/com/diamond_shop/diamond_shop/pojo/DetailDiamondPojo.java
@@ -0,0 +1,12 @@
+package com.diamond_shop.diamond_shop.pojo;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class DetailDiamondPojo {
+    
+}
