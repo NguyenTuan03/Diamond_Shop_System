98f895b0724a15708da73cb5b8a1944b4d8ead37
Lâm Tiên Hưng <89993051+gs4lthung@users.noreply.github.com>
2024-07-04 19:36:09 +0700

    refactor: remove unnecessary import

--- main/BackEnd/diamond_shop/src/main/java/com/diamond_shop/diamond_shop/config/SecurityConfig.java
+++ main/BackEnd/diamond_shop/src/main/java/com/diamond_shop/diamond_shop/config/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests(authorizeRequests ->
+                    .authorizeRequests(authorizeRequests ->
                         authorizeRequests
                                 .requestMatchers(HttpMethod.GET, "/api/").permitAll()
                                 .requestMatchers(HttpMethod.POST, "/api/").permitAll()
--- main/BackEnd/diamond_shop/src/main/java/com/diamond_shop/diamond_shop/entity/AccountEntity.java
+++ main/BackEnd/diamond_shop/src/main/java/com/diamond_shop/diamond_shop/entity/AccountEntity.java
@@ -6,11 +6,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.security.core.GrantedAuthority;
 
-import java.util.Collection;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @Entity
