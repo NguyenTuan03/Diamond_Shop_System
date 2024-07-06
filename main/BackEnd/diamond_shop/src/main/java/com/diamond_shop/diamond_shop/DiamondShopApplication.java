adf2b4ee1f80434d920de492a1e3b44231c1a05e
Lâm Tiên Hưng <89993051+gs4lthung@users.noreply.github.com>
2024-07-06 19:28:24 +0700

    fix: warning output

--- main/BackEnd/diamond_shop/src/main/java/com/diamond_shop/diamond_shop/DiamondShopApplication.java
+++ main/BackEnd/diamond_shop/src/main/java/com/diamond_shop/diamond_shop/DiamondShopApplication.java
@@ -2,10 +2,11 @@ package com.diamond_shop.diamond_shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
 import java.util.TimeZone;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { UserDetailsServiceAutoConfiguration.class })
 public class DiamondShopApplication {
 
     public static void main(String[] args) {
