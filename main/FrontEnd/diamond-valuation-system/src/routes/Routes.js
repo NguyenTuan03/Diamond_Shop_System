dc7b062d6774f1a9b03fe77fb2cf7af04916385d
Lâm Tiên Hưng <89993051+gs4lthung@users.noreply.github.com>
2024-07-11 16:24:51 +0700

    feat: add lazyload

--- main/FrontEnd/diamond-valuation-system/src/routes/Routes.js
+++ main/FrontEnd/diamond-valuation-system/src/routes/Routes.js
@@ -1,239 +1,247 @@
+import { lazy } from "react";
 import routes from "../config/Config.js";
-import DiamondCalculate from "../pages/calculate/DiamondCalculate.jsx";
-import DiamondCheck from "../pages/check/DiamondCheck.jsx";
-import DiamondCheckDetails from "../pages/check/DiamondCheckDetails.jsx";
-import Detail from "../pages/detail/Detail.jsx";
-import DiamondService from "../pages/services/DiamondService.jsx";
-import DiamondValuationRequest from "../pages/valuate/DiamondValuationRequest.jsx";
-import EducationCarat from "../pages/education/EducationCarat.jsx";
-import EducationCertificate from "../pages/education/EducationCertificate.jsx";
-import EducationClarity from "../pages/education/EducationClarity.jsx";
-import EducationColor from "../pages/education/EducationColor.jsx";
-import EducationCut from "../pages/education/EducationCut.jsx";
-import EducationShape from "../pages/education/EducationShape.jsx";
-import HomePage from "../pages/Home/Home.jsx";
-import Login from "../pages/login/Login.jsx";
-import Logout from "../pages/logout/Logout.jsx";
-import Prices from "../pages/prices/Prices.jsx";
-import Error from "../pages/error/Error.jsx";
-import DashBoardLayout from "../layout/DashBoardLayout.jsx";
-import DashBoard from "../pages/dashboard/DashBoard.jsx";
-import DashBoardNotification from "./../pages/dashboard/DashBoardNotification";
-import DashBoardPrices from "./../pages/dashboard/DashBoardPrices";
-import DashBoardAppoint from "./../pages/dashboard/DashBoardAppoint";
-import DashBoardSearch from "./../pages/dashboard/DashBoardSearch";
-import DashBoardRequest from "./../pages/dashboard/DashBoardRequest";
-import AboutUs from "../pages/aboutUs/AboutUs.jsx";
-import DashBoardSetting from "../pages/dashboard/DashBoardSetting.jsx";
-import DashBoardTransaction from "../pages/dashboard/DashBoardTransaction.jsx";
-import PendingRequestTable from "../pages/dashboard/table/PendingRequestTable.jsx";
-import { layout } from "@chakra-ui/react";
-import ProcessRequestTable from "../pages/dashboard/table/ProcessRequestTable.jsx";
-import SealingLetterTable from "../pages/dashboard/table/SealingLetterTable.jsx";
-import CommitmentTable from "../pages/dashboard/table/CommitmentTable.jsx";
-import ValuationStaffDashboard from "../pages/dashboard/valuation/ValuationStaffDashBoard.jsx";
-import AdminPage from "../pages/admin/AdminPage.jsx";
-import ServiceTable from "../pages/dashboard/table/ServiceTable.jsx";
-import Activate from "../pages/signUp/Activate.jsx";
-import ForgotPassword from "../pages/login/ForgotPassword.jsx";
-import ResetForgetPassword from "../pages/login/ResetForgetPassword.jsx";
+const DiamondCalculate = lazy(() =>
+  import("../pages/calculate/DiamondCalculate.jsx")
+);
+const DiamondCheck = lazy(() => import("../pages/check/DiamondCheck.jsx"));
+const DiamondCheckDetails = lazy(() =>
+  import("../pages/check/DiamondCheckDetails.jsx")
+);
+const Detail = lazy(() => import("../pages/detail/Detail.jsx"));
+const DiamondService = lazy(() =>
+  import("../pages/services/DiamondService.jsx")
+);
+const DiamondValuationRequest = lazy(() =>
+  import("../pages/valuate/DiamondValuationRequest")
+);
+const EducationCarat = lazy(() => import("../pages/education/EducationCarat.jsx"));
+const EducationCertificate = lazy(() => import("../pages/education/EducationCertificate.jsx"));
+const EducationClarity = lazy(() => import("../pages/education/EducationClarity.jsx"));
+const EducationColor = lazy(() => import("../pages/education/EducationColor.jsx"));
+const EducationCut = lazy(() => import("../pages/education/EducationCut.jsx"));
+const EducationShape = lazy(() => import("../pages/education/EducationShape.jsx"));
+const HomePage = lazy(() => import("../pages/Home/Home.jsx"));
+const Login = lazy(() => import("../pages/login/Login.jsx"));
+const Logout = lazy(() => import("../pages/logout/Logout.jsx"));
+const Prices = lazy(() => import("../pages/prices/Prices.jsx"));
+const Error = lazy(() => import("../pages/error/Error.jsx"));
+const DashBoardLayout = lazy(() => import("../layout/DashBoardLayout.jsx"));
+const DashBoard = lazy(() => import("../pages/dashboard/DashBoard.jsx"));
+const DashBoardNotification = lazy(() => import("../pages/dashboard/DashBoardNotification.jsx"));
+const DashBoardPrices = lazy(() => import("../pages/dashboard/DashBoardPrices.jsx"));
+const DashBoardAppoint = lazy(() => import("../pages/dashboard/DashBoardAppoint.jsx"));
+const DashBoardSearch = lazy(() => import("../pages/dashboard/DashBoardSearch.jsx"));
+const DashBoardRequest = lazy(() => import("../pages/dashboard/DashBoardRequest.jsx"));
+const AboutUs = lazy(() => import("../pages/aboutUs/AboutUs.jsx"));
+const DashBoardSetting = lazy(() => import("../pages/dashboard/DashBoardSetting.jsx"));
+const DashBoardTransaction = lazy(() => import("../pages/dashboard/DashBoardTransaction.jsx"));
+const PendingRequestTable = lazy(() => import("../pages/dashboard/table/PendingRequestTable.jsx"));
+const ProcessRequestTable = lazy(() => import("../pages/dashboard/table/ProcessRequestTable.jsx"));
+const SealingLetterTable = lazy(() => import("../pages/dashboard/table/SealingLetterTable.jsx"));
+const CommitmentTable = lazy(() => import("../pages/dashboard/table/CommitmentTable.jsx"));
+const ValuationStaffDashboard = lazy(() => import("../pages/dashboard/valuation/ValuationStaffDashBoard.jsx"));
+const AdminPage = lazy(() => import("../pages/admin/AdminPage.jsx"));
+const ServiceTable = lazy(() => import("../pages/dashboard/table/ServiceTable.jsx"));
+const Activate = lazy(() => import("../pages/signUp/Activate.jsx"));
+const ForgotPassword = lazy(() => import("../pages/login/ForgotPassword.jsx"));
+const ResetForgetPassword = lazy(() => import("../pages/login/ResetForgetPassword.jsx"));
 
 const ALlRoutes = [
-    {
-        path: routes.home,
-        component: HomePage,
-    },
-    {
-        path: routes.diamondCheck,
-        component: DiamondCheck,
-    },
-    {
-        path: routes.diamondCheckDetails,
-        component: DiamondCheckDetails,
-    },
-    {
-        path: routes.diamondDetail,
-        component: Detail,
-    },
+  {
+    path: routes.home,
+    component: HomePage,
+  },
+  {
+    path: routes.diamondCheck,
+    component: DiamondCheck,
+  },
+  {
+    path: routes.diamondCheckDetails,
+    component: DiamondCheckDetails,
+  },
+  {
+    path: routes.diamondDetail,
+    component: Detail,
+  },
 
-    {
-        path: routes.diamondCalculate,
-        component: DiamondCalculate,
-    },
-    {
-        path: routes.diamondService,
-        component: DiamondService,
-    },
+  {
+    path: routes.diamondCalculate,
+    component: DiamondCalculate,
+  },
+  {
+    path: routes.diamondService,
+    component: DiamondService,
+  },
 
-    {
-        path: routes.educationShape,
-        component: EducationShape,
-    },
-    {
-        path: routes.educationCarat,
-        component: EducationCarat,
-    },
-    {
-        path: routes.educationColor,
-        component: EducationColor,
-    },
-    {
-        path: routes.educationCut,
-        component: EducationCut,
-    },
-    {
-        path: routes.educationClarity,
-        component: EducationClarity,
-    },
-    {
-        path: routes.login,
-        component: Login,
-    },
-    {
-        path: routes.logout,
-        component: Logout,
-    },
-    {
-        path: routes.error,
-        component: Error,
-        layout: null,
-    },
-    {
-        path: routes.aboutUs,
-        component: AboutUs,
-    },
+  {
+    path: routes.educationShape,
+    component: EducationShape,
+  },
+  {
+    path: routes.educationCarat,
+    component: EducationCarat,
+  },
+  {
+    path: routes.educationColor,
+    component: EducationColor,
+  },
+  {
+    path: routes.educationCut,
+    component: EducationCut,
+  },
+  {
+    path: routes.educationClarity,
+    component: EducationClarity,
+  },
+  {
+    path: routes.login,
+    component: Login,
+  },
+  {
+    path: routes.logout,
+    component: Logout,
+  },
+  {
+    path: routes.error,
+    component: Error,
+    layout: null,
+  },
+  {
+    path: routes.aboutUs,
+    component: AboutUs,
+  },
 
-    {
-        path: routes.diamondValuationRequest,
-        component: DiamondValuationRequest,
-    },
-    {
-        path: routes.prices,
-        component: Prices,
-    },
-    {
-        path: routes.educationCertificate,
-        component: EducationCertificate,
-    },
+  {
+    path: routes.diamondValuationRequest,
+    component: DiamondValuationRequest,
+  },
+  {
+    path: routes.prices,
+    component: Prices,
+  },
+  {
+    path: routes.educationCertificate,
+    component: EducationCertificate,
+  },
 
-    {
-        path: routes.educationShape,
-        component: EducationShape,
-    },
-    {
-        path: routes.educationCarat,
-        component: EducationCarat,
-    },
-    {
-        path: routes.educationColor,
-        component: EducationColor,
-    },
-    {
-        path: routes.educationCut,
-        component: EducationCut,
-    },
-    {
-        path: routes.educationClarity,
-        component: EducationClarity,
-    },
-    {
-        path: routes.login,
-        component: Login,
-    },
-    {
-        path: routes.logout,
-        component: Logout,
-    },
-    // Customer
-    {
-        path: routes.dashboard,
-        component: DashBoard,
-        layout: DashBoardLayout,
-    },
-    {
-        path: routes.dasboardNotification,
-        component: DashBoardNotification,
-        layout: DashBoardLayout,
-    },
-    {
-        path: routes.dashboardPrices,
-        component: DashBoardPrices,
-        layout: DashBoardLayout,
-    },
-    {
-        path: routes.dashboardAppoint,
-        component: DashBoardAppoint,
-        layout: DashBoardLayout,
-    },
-    {
-        path: routes.dashboardTransaction,
-        component: DashBoardTransaction,
-        layout: DashBoardLayout,
-    },
-    {
-        path: routes.dashboardSearch,
-        component: DashBoardSearch,
-        layout: DashBoardLayout,
-    },
-    {
-        path: routes.dashboardRequest,
-        component: DashBoardRequest,
-        layout: DashBoardLayout,
-    },
-    {
-        path: routes.dashboardSetting,
-        component: DashBoardSetting,
-    },
-    {
-        path: routes.manageAccount,
-        component: AdminPage,
-        layout: DashBoardLayout,
-    },
-    {
-        path: routes.pendingRequest,
-        component: PendingRequestTable,
-        layout: DashBoardLayout,
-    },
-    {
-        path: routes.processRequest,
-        component: ProcessRequestTable,
-        layout: DashBoardLayout,
-    },
-    {
-        path: routes.sealingLetter,
-        component: SealingLetterTable,
-        layout: DashBoardLayout,
-    },
-    {
-        path: routes.commitment,
-        component: CommitmentTable,
-        layout: DashBoardLayout,
-    },
-    {
-        path: routes.valuationDiamond,
-        component: ValuationStaffDashboard,
-        layout: DashBoardLayout,
-    },
-    {
-        path: routes.manageService,
-        component: ServiceTable,
-        layout: DashBoardLayout,
-    },
-    {
-        path: routes.activate,
-        component: Activate,
-        layout: null,
-    },
-    {
-        path: routes.forgetPassword,
-        component: ForgotPassword,
-        layout: null,
-    },
-    {
-        path: routes.resetForgetPassword,
-        component: ResetForgetPassword,
-        layout: null,
-    },
+  {
+    path: routes.educationShape,
+    component: EducationShape,
+  },
+  {
+    path: routes.educationCarat,
+    component: EducationCarat,
+  },
+  {
+    path: routes.educationColor,
+    component: EducationColor,
+  },
+  {
+    path: routes.educationCut,
+    component: EducationCut,
+  },
+  {
+    path: routes.educationClarity,
+    component: EducationClarity,
+  },
+  {
+    path: routes.login,
+    component: Login,
+  },
+  {
+    path: routes.logout,
+    component: Logout,
+  },
+  // Customer
+  {
+    path: routes.dashboard,
+    component: DashBoard,
+    layout: DashBoardLayout,
+  },
+  {
+    path: routes.dasboardNotification,
+    component: DashBoardNotification,
+    layout: DashBoardLayout,
+  },
+  {
+    path: routes.dashboardPrices,
+    component: DashBoardPrices,
+    layout: DashBoardLayout,
+  },
+  {
+    path: routes.dashboardAppoint,
+    component: DashBoardAppoint,
+    layout: DashBoardLayout,
+  },
+  {
+    path: routes.dashboardTransaction,
+    component: DashBoardTransaction,
+    layout: DashBoardLayout,
+  },
+  {
+    path: routes.dashboardSearch,
+    component: DashBoardSearch,
+    layout: DashBoardLayout,
+  },
+  {
+    path: routes.dashboardRequest,
+    component: DashBoardRequest,
+    layout: DashBoardLayout,
+  },
+  {
+    path: routes.dashboardSetting,
+    component: DashBoardSetting,
+  },
+  {
+    path: routes.manageAccount,
+    component: AdminPage,
+    layout: DashBoardLayout,
+  },
+  {
+    path: routes.pendingRequest,
+    component: PendingRequestTable,
+    layout: DashBoardLayout,
+  },
+  {
+    path: routes.processRequest,
+    component: ProcessRequestTable,
+    layout: DashBoardLayout,
+  },
+  {
+    path: routes.sealingLetter,
+    component: SealingLetterTable,
+    layout: DashBoardLayout,
+  },
+  {
+    path: routes.commitment,
+    component: CommitmentTable,
+    layout: DashBoardLayout,
+  },
+  {
+    path: routes.valuationDiamond,
+    component: ValuationStaffDashboard,
+    layout: DashBoardLayout,
+  },
+  {
+    path: routes.manageService,
+    component: ServiceTable,
+    layout: DashBoardLayout,
+  },
+  {
+    path: routes.activate,
+    component: Activate,
+    layout: null,
+  },
+  {
+    path: routes.forgetPassword,
+    component: ForgotPassword,
+    layout: null,
+  },
+  {
+    path: routes.resetForgetPassword,
+    component: ResetForgetPassword,
+    layout: null,
+  },
 ];
 
 export { ALlRoutes };
