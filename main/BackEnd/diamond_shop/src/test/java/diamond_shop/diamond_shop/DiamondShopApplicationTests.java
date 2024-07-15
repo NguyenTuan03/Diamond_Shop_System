433f101e6247982199bdfdf70adb5ee44c525c8e
Lâm Tiên Hưng <89993051+gs4lthung@users.noreply.github.com>
2024-07-15 18:42:57 +0700

    refactor: remove unnecessary import

--- main/BackEnd/diamond_shop/src/test/java/diamond_shop/diamond_shop/DiamondShopApplicationTests.java
+++ main/BackEnd/diamond_shop/src/test/java/diamond_shop/diamond_shop/DiamondShopApplicationTests.java
@@ -1,8 +1,6 @@
 package diamond_shop.diamond_shop;
 
-import com.diamond_shop.diamond_shop.DiamondShopApplication;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 
 class DiamondShopApplicationTests {
