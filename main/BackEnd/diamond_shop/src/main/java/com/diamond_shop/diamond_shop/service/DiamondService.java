39e20dcef984138ddde719b59a3a507f7cff315f
Lâm Tiên Hưng <89993051+gs4lthung@users.noreply.github.com>
2024-07-17 21:20:45 +0700

    feat: manager CRUD service

--- main/BackEnd/diamond_shop/src/main/java/com/diamond_shop/diamond_shop/service/DiamondService.java
+++ main/BackEnd/diamond_shop/src/main/java/com/diamond_shop/diamond_shop/service/DiamondService.java
@@ -1,6 +1,7 @@
 package com.diamond_shop.diamond_shop.service;
 
 import com.diamond_shop.diamond_shop.dto.UpdateServiceDTO;
+import com.diamond_shop.diamond_shop.pojo.ResponsePojo;
 import com.diamond_shop.diamond_shop.pojo.ServiceResultPojo;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -10,8 +11,15 @@ import java.util.List;
 @Service
 public interface DiamondService {
     ResponseEntity<String> fetchDiamondCalculate(String gradingLab, String carat, String shape, String color, String clarity, String cut);
-//
+
+    //
     List<ServiceResultPojo> getAllServices();
-    String updateService(UpdateServiceDTO updateServiceDTO);
+
+    ResponsePojo createService(UpdateServiceDTO updateServiceDTO);
+
+    ResponsePojo updateService(UpdateServiceDTO updateServiceDTO);
+
+    ResponsePojo deleteService(int serviceId, int statisticId);
+
 
 }
